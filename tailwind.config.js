54d923629a9727d324688f04521e1bd4bc2e0315
Jansen Cadorna <cadornajansen4757@gmail.com>
2023-02-26 21:01:37 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("@headlessui/tailwindcss")],
+};
